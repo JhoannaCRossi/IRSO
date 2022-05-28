df75597d41fc54b6036603480bed9c7cfd8ebcb8
jcastellanos <jcastellanos@zetti.com.ar>
2022-05-28 00:18:40 -0300

    class four practice

--- integracion_de_programacion/ClassFour/src/practice1/Main.java
+++ integracion_de_programacion/ClassFour/src/practice1/Main.java
@@ -1,5 +1,55 @@
 package practice1;
 
-public class Main {
+import java.util.Scanner;
 
+/* Realizar un programa que lea por teclado 20 valores. Que sólo tome
+ * valores entre 2 y 97, y que si es mayor o igual de 50 los vaya sumando, y
+ * en caso que sea menor los vaya restando cargando el resultado dentro de
+ * una variable llamada “resultado”
+ * */
+
+public class Main {
+	public static void main(String[] args){
+		//instance scanner
+		Scanner sc = new Scanner(System.in);
+		
+		String input;
+		int numberInput, resultado = 0;
+		
+		//declaring array and allocating memory to array
+		int[] array = new int[20];
+		
+		for (int i = 0; i < array.length; i++) {
+			do {          
+				System.out.println("Please , give me your "+(i+1)+" number! Please enter "
+						+ "numbers from 2 to 97");
+				input = sc.nextLine(); 
+			} while(!IsANumberOnRange(input)); //check number
+			
+			numberInput = Integer.parseInt(input);
+			
+			if(numberInput >= 50) {
+				resultado += numberInput;
+			}else {
+				resultado -= numberInput;
+			}
+			
+		}
+		System.out.println("\n");
+		System.out.println(resultado+" is the result!!");
+	}
+	
+	public static boolean IsANumberOnRange(String text) {
+	    int number;
+	    try {
+	    	number = Integer.parseInt(text);
+	    	if(number >= 2 && number <= 97) {
+	    		return true;	    		
+	    	}else {
+	    		return false;
+	    	}
+	    } catch (NumberFormatException ex) {
+	       return false;
+	    }
+	}
 }
--- integracion_de_programacion/ClassFour/src/practice2/Main.java
+++ integracion_de_programacion/ClassFour/src/practice2/Main.java
@@ -1,5 +1,46 @@
 package practice2;
 
-public class Main {
+import java.util.Scanner;
 
+/* Realiza un programa que lea de teclado valores entre 32 y 120, y los vaya
+ * sumando uno a uno dentro de la variable “sumatoria”. En el caso que sea
+ * fuera de ese rango que no realice ninguna operación. Y en caso que
+ * ingrese el valor cero “0”, debe finalizar el programa.
+ * */
+
+public class Main {
+	public static void main(String[] args){
+		//instance scanner
+		Scanner sc = new Scanner(System.in);
+		
+		String input;
+		int numberInput = 0, acum = 0, finish = 1;
+		
+		do {
+			do {          
+				System.out.println("Please , give me your number! Enter "
+						+ "numbers from 32 to 120; to finish enter 0");
+				input = sc.nextLine(); 
+			} while(!IsANumberOnRange(input)); //check number	
+			
+			numberInput = Integer.parseInt(input);
+			acum += numberInput;
+		}while(numberInput!=0);
+		
+		System.out.println("its done!");
+	}
+	
+	public static boolean IsANumberOnRange(String text) {
+	    int number;
+	    try {
+	    	number = Integer.parseInt(text);
+	    	if(number >= 32 && number <= 120 || number == 0) {
+	    		return true;	    		
+	    	}else {
+	    		return false;
+	    	}
+	    } catch (NumberFormatException ex) {
+	       return false;
+	    }
+	}
 }
