caf40f402fdb7f2eeb79c776cb1c0d4e72020661
jcastellanos <jcastellanos@zetti.com.ar>
2022-06-18 16:25:39 -0300

    exercises one and two of this class

---
+++ integracion_de_programacion/ClassSeventh/src/practice1/Main.java
@@ -0,0 +1,65 @@
+package practice1;
+
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+		final int N = 10;
+		String input;
+
+		// declaring array and allocating memory to array
+		int q[] = new int[N];
+
+		for (int i = 0; i < q.length; i++) {
+			do {
+				System.out.println("Please, give me your " + (i + 1) + " number! Please enter " + "numbers >= 0");
+				input = sc.nextLine();
+			} while (!isANumberOnRange(input)); // check number
+			q[i] = Integer.parseInt(input);
+		}
+
+		System.out.println("\n");
+		System.out.println("charged vector!!");
+
+		// print vector charged
+		for (int i : q) {
+			System.out.print(i + " ");
+		}
+		do {
+			System.out.println("\nPlease, give me the number to search!");
+			input = sc.nextLine();
+		} while (!isANumberOnRange(input)); // check number
+
+		int numberSearch = Integer.parseInt(input);
+		//find number
+		for (int j = 0; j < q.length; j++) {
+			if (q[j] == numberSearch) {
+				q[j] = 0;
+			}
+		}
+
+		// finally print vector modificated
+		System.out.println("\n");
+		for (int i : q) {
+			System.out.print(i + " ");
+		}
+		System.out.println("\n Is the result!! Finish.");
+	}
+
+	public static boolean isANumberOnRange(String text) {
+		int number;
+		try {
+			number = Integer.parseInt(text);
+			if (number >= 0) {
+				return true;
+			} else {
+				return false;
+			}
+		} catch (NumberFormatException ex) {
+			return false;
+		}
+	}
+}
---
+++ integracion_de_programacion/ClassSeventh/src/practice2/Main.java
@@ -0,0 +1,51 @@
+package practice2;
+
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		final int K = 5;
+		String input;
+		double acum = 0;
+
+		// declaring array and allocating memory to array
+		double z[] = new double[K];
+
+		for (int i = 0; i < z.length; i++) {
+			do {
+				System.out.println("Please, give me your " + (i + 1) + " number! Please enter " + "numbers >= 0");
+				input = sc.nextLine();
+			} while (!isANumberOnRange(input)); // check number
+			z[i] = Integer.parseInt(input);
+			acum+= Integer.parseInt(input);
+		}
+
+		System.out.println("\n");
+		System.out.println("Charged vector!!");
+
+		// print vector charged
+		for (double i : z) {
+			System.out.print(i + " ");
+		}
+		
+		System.out.println("\n");
+		System.out.println("The sum of the elements of this vector is "+acum);
+
+	}
+	
+	public static boolean isANumberOnRange(String text) {
+		int number;
+		try {
+			number = Integer.parseInt(text);
+			if (number >= 0) {
+				return true;
+			} else {
+				return false;
+			}
+		} catch (NumberFormatException ex) {
+			return false;
+		}
+	}
+}
